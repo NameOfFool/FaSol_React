06cfff54f91cf63dda0361ad4993f979a1326a1b
NameOfFool <ivan.pauk2008@gmail.com>
2022-10-18 15:53:53 +0300

    18.10.22

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,14 @@ import logo from "../src/logo.png"
 function App() {
   return (
     <div className="App">
-      <header className="bg-[#ff1e1e] px-2 py-2.5 fixed top-0 w-full">
-        <nav className='container flex flex-wrap justify-between items-center mx-auto'>
+      <header className="rounded border-grey-200 px-2 sm:px-4 py-2.5">
+        <nav className='container flex flex-wrap justify-between items-center mx-auto '>
           <img src={logo} className="logo h-20"></img>
+          <ul className="flex flex-row p-4 bg-gray-50 rounded-lg">
+            <li><a href='#' className="block py-2 pr-4 lo-3 text-gray-700 rounded hover:bg-gray-100">Главная</a></li>
+            <li><a href='#' className="block py-2 pr-4 lo-3 text-gray-700 rounded hover:bg-gray-100">Вход</a></li>
+            <li><a href='#' className="block py-2 pr-4 lo-3 text-gray-700 rounded hover:bg-gray-100">Регистрация</a></li>
+          </ul>
         </nav>
       </header>
     </div>
