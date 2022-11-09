41e131df2bb88fec0d871e5f63e55db59b8456e5
NameOfFool <ivan.pauk2008@gmail.com>
2022-11-09 15:53:50 +0300

    middleware

--- server/router/index.js
+++ server/router/index.js
@@ -2,12 +2,17 @@ const Router = require("express").Router;
 const userController = require("../controllers/userController");
 const UserController = require("../controllers/userController")
 const router = Router();
+const authMiddleware = require("../middlewares/auth_middleware")
+const { body } = require("express-validator")
 
-router.post('/registration', userController.registration);
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({ min: 3, map: 15 }),
+    userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 module.exports = router;
